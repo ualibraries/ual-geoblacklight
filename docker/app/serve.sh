e4a8545e8458c90b244d6ac93eb84d6216ba6af2
Jeff Turman <turmanator@email.arizona.edu>
2023-02-28 21:56:44 -0700

    udpates for restart of container

--- docker/app/serve.sh
+++ docker/app/serve.sh
@@ -10,10 +10,19 @@ rvm install ${RUBY_VER}
 gem install bundler rails
 
 DISABLE_SPRING=1
+APP_DIR="/geoblacklight/ual_geoblacklight"
 
-cd /geoblacklight
-rails new ual_geoblacklight -m https://raw.githubusercontent.com/geoblacklight/geoblacklight/main/template.rb
-
-cd ual_geoblacklight
-echo 'gem "devise"' >> Gemfile
-rake geoblacklight:server["-p 3000 -b 0.0.0.0"]
+if [[ ! -d "${APP_DIR}" ]]; then
+    # create a new instance
+    cd /geoblacklight
+    rails new ual_geoblacklight -m https://raw.githubusercontent.com/geoblacklight/geoblacklight/main/template.rb
+    cd ual_geoblacklight
+    echo 'gem "devise"' >> Gemfile
+    rake geoblacklight:server["-p 3000 -b 0.0.0.0"]
+else
+    # restart both servers since we have apparently done this before
+    cd "/tmp/solr-${SOLR_VER}"
+    bin/solr start
+    cd $APP_DIR
+    rails s -b 0.0.0.0
+fi
