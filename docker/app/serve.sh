ae4a1c6942964d161f990d9e5ffbfdc41836c441
Jeff Turman <turmanator@email.arizona.edu>
2023-04-12 16:27:03 -0700

    update server config

--- docker/app/serve.sh
+++ docker/app/serve.sh
@@ -11,7 +11,7 @@ if [[ ! -f "${ROOT_DIR}/.docker_init_flag" ]]; then
     touch "${ROOT_DIR}/.docker_init_flag"
 
     if [[ ! -d "${ROOT_DIR}/app/.bundle" ]]; then
-        mkdir app/.bundle && chmod 755 app/.bundle
+        mkdir "${ROOT_DIR}/app/.bundle" && chmod 755 "${ROOT_DIR}/app/.bundle"
     fi
 
     cd "${ROOT_DIR}/app"
@@ -24,7 +24,7 @@ else
     # restart both servers since we have apparently done this before
     # GOB server is too dumb to start with the same command as above
 
-    cd "${ROOT_DIR}/docker/solr"
+    cd "${ROOT_DIR}/app/docker/solr"
     bin/solr start -h "0.0.0.0" -p 8983 -V
 
     cd "${ROOT_DIR}/app"
