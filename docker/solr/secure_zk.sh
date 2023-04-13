bdc24ed4a60c56baa7246d41fca31914c5bfef23
Jeff Turman <turmanator@email.arizona.edu>
2023-04-12 20:58:19 -0700

    add solr restart to secure script

--- docker/solr/secure_zk.sh
+++ docker/solr/secure_zk.sh
@@ -5,3 +5,6 @@ source ~/.profile
 
 cd /geoblacklight/app/docker/solr/server/scripts/cloud-scripts
 source zkcli.sh -zkhost zoo1:2181 -cmd putfile /security.json /home/gob/docker/solr/security.json
+
+cd /geoblacklight/app/docker/solr
+bin/solr restart
