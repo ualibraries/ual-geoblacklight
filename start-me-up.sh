f9716032ef1fa3556ad677eb6a6c459458236a9f
Jeff Turman <turmanator@email.arizona.edu>
2023-04-19 10:22:48 -0700

    add conditional logic to start up script

--- start-me-up.sh
+++ start-me-up.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 set -x
 
-env USER_GID=$(id -g) USER_UID=$(id -u) docker-compose up -d
+
+export USER_GID=$(id -g)
+export USER_UID=$(id -u)
+
+APP_RUNNING="$(docker-compose ps --status=running ual_gob_app | grep 'gob-test')"
+
+if [[ $1 == "pause" && "${APP_RUNNING}" ]]; then
+    docker-compose stop
+elif [[ "${APP_RUNNING}" ]]; then
+    docker-compose restart
+else
+    docker-compose up -d
+fi
+
+if [[ ! -d ./ual_gob/app ]]; then
+    docker exec -it gob-test bash -c -l 'cd app && ./install_app.sh'
+fi
