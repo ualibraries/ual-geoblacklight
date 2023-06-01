d0f0cc5b82b638689ae7510741a5d9f2a9f9d464
Jeff Turman <turmanator@email.arizona.edu>
2023-06-01 09:38:02 -0700

    update image build script

--- dbuild.sh
+++ dbuild.sh
@@ -3,5 +3,13 @@
 source .env
 
 cd docker
-docker build --rm=true -t ual-goblight:latest -f app/Dockerfile .
-docker build --rm=true -t ual-solr:latest --build-arg BASE_IMAGE_VERSION=${SOLR_VERSION} -f solr/Dockerfile .
+
+docker build --rm=true -t ual-goblight:latest \
+    --build-arg RUBY_VERSION=${RUBY_VERSION} \
+    --build-arg RAILS_VERSION=${RAILS_VERSION} \
+    --build-arg VIEW_COMPONENT_VERSION=${VIEW_COMPONENT_VERSION} \
+    -f app/Dockerfile .
+
+docker build --rm=true -t ual-solr:latest \
+    --build-arg SOLR_VERSION=${SOLR_VERSION} \
+    -f solr/Dockerfile .
