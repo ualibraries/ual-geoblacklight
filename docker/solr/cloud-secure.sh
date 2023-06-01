59c8c193eab371f9f3c22ee1e42530064a45fdbd
Jeff Turman <turmanator@email.arizona.edu>
2023-06-01 10:17:12 -0700

    update security to block admin access by default

--- docker/solr/cloud-secure.sh
+++ docker/solr/cloud-secure.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
+# this script controls solr startup tasks. 
+# this script may changed to add more security, etc.
+
 if [[ ! -n "$(ls -d /var/solr/data/blacklight-core*)" ]]; then
     echo -e "\nCreating GOB Solr core...\n"
     solr-create -c blacklight-core -d /blacklight-core
@@ -9,5 +13,6 @@ echo -e "\nSetting up and securing SolrCloud install with basic authentication..
 cd /opt/solr
 bin/solr zk cp file:/opt/solr/server/solr/security.json zk:/security.json
 
+# Don't add anything else below this comment
 echo -e "\nRestarting the Solr server now...\n"
 solr-foreground
