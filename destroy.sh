23768ecb251520cedda5da4c1a0690cc8523715e
Jeff Turman <turmanator@email.arizona.edu>
2023-10-25 10:43:51 -0700

    Add logic for destroy clean up

--- destroy.sh
+++ destroy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 env USER_GID=$(id -g) USER_UID=$(id -u) docker compose down -v
+
+echo -ne "Do you want to remove the following files? (y|n) \n"
+git clean -fdxn
+read option
+if [ "$option" == "y" ]; then
+    git clean -fdx
+elif [ "$option" == "n" ]; then
+    echo -e "\nNot removing any untracked files.\n"
+else 
+    echo "Sorry, I could not understand the input, so no files were removed."
+fi
