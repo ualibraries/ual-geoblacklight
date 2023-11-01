1dd565e00a185fb2e9b8bb3278ef7cc8c612273c
Jeff Turman <turmanator@email.arizona.edu>
2023-11-01 15:49:32 -0700

    Add optional line number

--- inspect-app-logs.sh
+++ inspect-app-logs.sh
@@ -3,4 +3,4 @@
 
 # script to tail app logs for development
 
-docker exec -it gob-app bash -c -l 'cd /geoblacklight/app/log && tail -f -n 50 development.log'
+docker exec -it gob-app bash -c -l "cd /geoblacklight/app/log && tail -f -n ${@:-10} development.log"
