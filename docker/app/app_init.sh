5ec60e343ec5c0e38e144cd749db4d3350d34110
Jeff Turman <turmanator@email.arizona.edu>
2023-11-01 15:40:49 -0700

    Update comment doc

--- docker/app/app_init.sh
+++ docker/app/app_init.sh
@@ -2,6 +2,9 @@
 set -e
 
 
+# RVM is installed at container build time, but RoR is installed here.
+# We don't use the templated install, so we can use the (faster) setup script.
+
 source ~/.rvm/scripts/rvm
 
 cd ${APP_DIR}
