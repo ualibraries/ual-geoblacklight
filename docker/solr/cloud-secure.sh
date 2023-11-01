2a6484af0400745b86886621c31bee992909e629
Jeff Turman <turmanator@email.arizona.edu>
2023-11-01 15:41:32 -0700

    Update comment docs

--- docker/solr/cloud-secure.sh
+++ docker/solr/cloud-secure.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 
-# this script controls solr startup tasks. 
-# this script may changed to add more security, etc.
+# This script controls solr startup tasks and may be changed to add more security, etc.
+# Auth credentials come from a couple different locations:
+# solr creds: .env (built into container)
+# zk creds: docker/solr/solr.in.sh (also built in)
+# Index shard number can be increased in the solr-create command below.
 
 if [[ ! -n "$(ls -d /var/solr/data/blacklight-core*)" ]]; then
     echo -e "\nCreating GOB Solr core...\n"
@@ -11,9 +14,8 @@ fi
 
 echo -e "\nSetting up and securing SolrCloud install with basic authentication...\n"
 cd /opt/solr
-# bin/solr zk cp file:/opt/solr/server/solr/security.json zk:/security.json
 bin/solr auth enable -type basicAuth -credentials ${SOLR_AUTH} -zkHost zoo1:2181 -blockUnknown true
 
-# Don't add anything else below this comment
+# Don't add anything else below this comment.
 echo -e "\nRestarting the Solr server now...\n"
 solr-foreground
