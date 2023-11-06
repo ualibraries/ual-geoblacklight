3a7e5bfb6ba4d05186563ae520b9873fe60379a5
Jeff Turman <turmanator@email.arizona.edu>
2023-11-06 10:00:51 -0700

    Add whitespace to destroy dialogue

--- destroy.sh
+++ destroy.sh
@@ -4,17 +4,18 @@ docker compose down -v
 
 FILES=`git clean -fdxn`
 if [ -n "${FILES}" ]; then
-    echo -ne "Do you want to remove the following files? (y|n) \n"
-    echo "${FILES}"
+    echo -e "\n\nThe following files have untracked changes since the last commit, \nusually meaning they are ignored and/or code-generated dependencies. \n\n"
+    echo "${FILES} \n\n"
+    echo -e "Do you want to remove them? (y|n)"
     read option
     if [ "$option" == "y" ]; then
         git clean -fdx
     elif [ "$option" == "n" ]; then
-        echo -e "\nNot removing any untracked files.\n"
+        echo -e "\n\nNot removing any untracked files. This may have an effect on the next container startup. \n\n"
     else
-        echo -e "\nSorry, I could not understand the input.\n"
+        echo -e "\n\nSorry, I could not understand the input. \n\n"
     fi
 else
-    echo -e "\nNo untracked files were found.\n"
+    echo -e "\n\nNo untracked files were found. \n\n"
     exit 0
 fi
