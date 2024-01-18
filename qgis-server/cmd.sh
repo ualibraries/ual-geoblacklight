6bd12d0fb1e486969d71705a3542e80f6097ce09
Jeff Turman <turmanator@email.arizona.edu>
2024-01-18 08:50:41 -0700

    Initial commit of geoserver/postgis docker assets

--- qgis-server/cmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[[ $DEBUG == "1" ]] && env
-
-exec /usr/bin/xvfb-run --auto-servernum --server-num=1 /usr/bin/spawn-fcgi -p 5555 -n -d /home/qgis -- /usr/lib/cgi-bin/qgis_mapserv.fcgi
