051146b32de968d974ae641efa969e9fed598793
Jeff Turman <turmanator@email.arizona.edu>
2024-02-06 10:11:16 -0700

    Add init script command to geoserver startup stanza

--- start-me-up.sh
+++ start-me-up.sh
@@ -17,8 +17,9 @@ elif [[ $1 == "test" && !"${APP_RUNNING}" ]]; then
 
 elif [[ $1 == "geoserver" && !"${APP_RUNNING}" ]]; then
 
-    echo -e "Starting the Docker orchestrationwith geoserver container and postgis startup...\n"
+    echo -e "Starting the Docker orchestration with geoserver container and postgis startup...\n"
     docker compose -f docker-compose.yml -f ./docker/geoserver/docker-compose.yml -f ./docker/postgis/docker-compose.yml up -d
+    docker exec -it gob-app bash -c -l './app_init.sh'
 
 elif [[ "${APP_RUNNING}" ]]; then
 
