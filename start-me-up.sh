1351da5fb7d116f03dfde84faa6c82f0262eda1f
Jeff Turman <turmanator@email.arizona.edu>
2024-02-23 12:28:54 -0700

    Update startup script for traefik network

--- start-me-up.sh
+++ start-me-up.sh
@@ -13,12 +13,14 @@ if [[ $1 == "pause" && "${APP_RUNNING}" ]]; then
 elif [[ $1 == "test" && !"${APP_RUNNING}" ]]; then
 
     echo -e "Starting the Docker orchestration without app install and server startup...\n"
+    docker network create -d bridge ual-gob
     docker compose up -d
 
 elif [[ $1 == "geoserver" && !"${APP_RUNNING}" ]]; then
 
     echo -e "Starting the Docker orchestration with geoserver container and postgis startup...\n"
-    docker compose -f docker-compose.yml -f ./docker/geoserver/docker-compose.yml -f ./docker/postgis/docker-compose.yml up -d
+    docker network create -d bridge ual-gob
+    docker compose -f ./docker-compose.yml -f ./docker/geoserver/docker-compose.yml up -d
     docker exec -it gob-app bash -c -l './app_init.sh'
 
 elif [[ "${APP_RUNNING}" ]]; then
@@ -29,6 +31,8 @@ elif [[ "${APP_RUNNING}" ]]; then
 
 else
 
+    "Starting the Docker orchestration with app install and server startup...\n"
+    docker network create -d bridge ual-gob
     docker compose up -d
     docker exec -it gob-app bash -c -l './app_init.sh'
 
