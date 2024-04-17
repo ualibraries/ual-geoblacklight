e853c80f679bd3f948dc1638223173f9ac2e7818
Jeff Turman <turmanator@email.arizona.edu>
2024-04-17 16:08:43 -0700

    Update for using ssh keys inside app container. Refs #15628

--- docker/app/app_init.sh
+++ docker/app/app_init.sh
@@ -2,6 +2,9 @@
 set -e
 
 
+# Change the ownership of SSH keys to allow internal deployments
+chown -R gob:gob /home/gob/.ssh
+
 # RVM is installed at container build time, but RoR is installed here.
 # We don't use the templated install, so we can use the (faster) setup script.
 
