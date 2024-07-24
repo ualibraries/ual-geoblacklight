f6f3e73c7cbaf1656366199395b288f05d8c2c3c
Andy Osborne <cao89@arizona.edu>
2024-07-24 16:54:42 -0700

    Fix git clean command to only run in app directory refs #15628

--- destroy.sh
+++ destroy.sh
@@ -2,14 +2,14 @@
 
 docker compose down -v
 
-FILES=`git clean -fdxn`
+FILES=`git clean -fdxn app`
 if [ -n "${FILES}" ]; then
     echo -e "\n\nThe following files have untracked changes since the last commit, \nusually meaning they are ignored and/or code-generated dependencies. \n\n"
     echo "${FILES}"
     echo -e "\nDo you want to remove them? (y|n)"
     read option
     if [ "$option" == "y" ]; then
-        git clean -fdx
+        git clean -fdx app
     elif [ "$option" == "n" ]; then
         echo -e "\n\nNot removing any untracked files. This may have an effect on the next container startup. \n\n"
     else
