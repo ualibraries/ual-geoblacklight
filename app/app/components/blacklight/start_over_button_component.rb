32b61fa46b84cdc791b6e7bb40a06f5ecda2e64b
Ashley Slay <aslay@arizona.edu>
2024-07-26 12:32:37 -0700

    Allow unicode in buttons, refs #15911

--- app/app/components/blacklight/start_over_button_component.rb
+++ app/app/components/blacklight/start_over_button_component.rb
@@ -3,7 +3,7 @@
 module Blacklight
   class StartOverButtonComponent < Blacklight::Component
     def call
-      link_to t('blacklight.search.start_over'), start_over_path, class: 'catalog_startOverLink btn btn-primary'
+      link_to t('blacklight.search.start_over').html_safe, start_over_path, class: 'catalog_startOverLink btn btn-outline-blue'
     end
 
     private
--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -24,5 +24,37 @@ module ApplicationHelper
     end
   end
   deprecation_deprecate link_to_previous_document: 'Moving to Blacklight::SearchContextComponent'
+
+  # Create a link back to the index screen, keeping the user's facet, query and paging choices intact by using session.
+  # @example
+  #   link_back_to_catalog(label: 'Back to Search')
+  #   link_back_to_catalog(label: 'Back to Search', route_set: my_engine)
+  def ual_link_back_to_catalog(opts = { label: nil })
+    scope = opts.delete(:route_set) || self
+    query_params = search_state.reset(current_search_session.try(:query_params)).to_hash
+
+    if search_session['counter']
+      per_page = (search_session['per_page'] || blacklight_config.default_per_page).to_i
+      counter = search_session['counter'].to_i
+
+      query_params[:per_page] = per_page unless search_session['per_page'].to_i == blacklight_config.default_per_page
+      query_params[:page] = ((counter - 1) / per_page) + 1
+    end
+
+    link_url = if query_params.empty?
+                 search_action_path(only_path: true)
+               else
+                 scope.url_for(query_params)
+               end
+    label = opts.delete(:label)
+
+    if link_url =~ /bookmarks/
+      label ||= t('blacklight.back_to_bookmarks').html_safe
+    end
+
+    label ||= t('blacklight.back_to_search').html_safe
+
+    link_to label, link_url, opts
+  end
   
 end
