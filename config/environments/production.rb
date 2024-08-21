72bec315097f6db96326ca85e43ce533a2f1a245
Ashvini Kabadi <ashvinikabadi@arizona.edu>
2024-08-21 16:19:00 -0700

    WIP - Added bin/yarn in setup file to run javascript dependency refs #15907

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
-
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
