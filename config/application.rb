6ae1b216868270993258096548186b420f2bc469
Ashvini Kabadi <ashvinikabadi@arizona.edu>
2024-08-23 10:50:59 -0700

    WIP - Added yarn.rake task  to run javascript dependency refs #15907

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module App
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.autoload_paths += %W(#{config.root}/lib)
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
