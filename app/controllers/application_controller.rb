d8e4ad4183eb75a8e8e5c4604b83eb16bf9367ba
Ashvini Kabadi <ashvinikabadi@arizona.edu>
2025-02-20 11:27:45 -0500

    WIP : Configure devise to use only the modules we want Refs#16367

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,16 @@ class ApplicationController < ActionController::Base
           blacklight_config.search_state_fields.append(Settings.GBL_PARAMS)
         end
 
+        # Alias new_session_path as login_path for default devise config
+        def new_user_session_path(*)
+          user_shibd_omniauth_authorize_path
+        end
+
+        def after_sign_in_path_for(resource)
+          request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+        end
+
+
   # Status page for Pingdom
   def status
     output = {
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  }, skip: [:sessions, :registrations, :passwords]
 
   concern :exportable, Blacklight::Routes::Exportable.new
 
