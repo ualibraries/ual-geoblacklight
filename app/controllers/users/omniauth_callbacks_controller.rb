a45de0ed1cd9de45968dcfa748f7063a9ec0561e
Ashvini Kabadi <ashvinikabadi@arizona.edu>
2025-03-04 15:56:27 -0500

    Only allow a subset of NetIDs to authenticate to the app Refs #16383

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,6 +14,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    redirect_to root_path, alert: "Authentication failed!"
+    reason = request.env["omniauth.error.message"]
+    redirect_to stored_location_for(:user) || root_path, alert: I18n.t("devise.omniauth_callbacks.failure", kind: "Shibboleth", reason: reason)
   end
+
 end
