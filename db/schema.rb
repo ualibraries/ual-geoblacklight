f2cad36dd9686b734e6c729fa250d80fd5a0ad9c
Ashley SlayMoore <aslay@arizona.edu>
2025-07-24 11:37:32 -0700

    Update schema, refs #16667

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_23_005309) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_24_183600) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_type"
